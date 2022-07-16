7879a5407c3901ef5c6af1ba5eeb3a775b7397b0
abdalrhmansami <abdalrhmansami5@gmail.com>
2022-07-16 19:46:27 +0300

    add red overly to work

--- src/pages/home/latest-work/LatestWork.tsx
+++ src/pages/home/latest-work/LatestWork.tsx
@@ -13,19 +13,64 @@ const LatestWork = () => {
       </div>
       <section className="photo-work">
         <Zoom bottom>
-          <div className="pic-work pic1-work"></div>
+          <div className="parent-work-pic">
+            <div className="pic-work pic1-work">
+              <div className="red-overly">
+                <div className="caption-work">
+                  <h2>Central Office</h2>
+                  <p>OFFICES</p>
+                </div>
+              </div>
+            </div>
+          </div>
         </Zoom>
         <Zoom bottom>
-          <div className="pic-work pic2-work"></div>
+          <div className="parent-work-pic">
+            <div className="pic-work pic2-work">
+              <div className="red-overly">
+                <div className="caption-work">
+                  <h2>Shopping center</h2>
+                  <p>SHOPPING</p>
+                </div>
+              </div>
+            </div>
+          </div>
         </Zoom>
         <Zoom bottom>
-          <div className="pic-work pic3-work"></div>
+          <div className="parent-work-pic">
+            <div className="pic-work pic3-work">
+              <div className="red-overly">
+                <div className="caption-work">
+                  <h2>Resident area</h2>
+                  <p>RESIDENT</p>
+                </div>
+              </div>
+            </div>
+          </div>
         </Zoom>
         <Zoom bottom>
-          <div className="pic-work pic4-work"></div>
+          <div className="parent-work-pic">
+            <div className="pic-work pic4-work">
+              <div className="red-overly">
+                <div className="caption-work">
+                  <h2>Office center</h2>
+                  <p>OFFICES</p>
+                </div>
+              </div>
+            </div>
+          </div>
         </Zoom>
         <Zoom bottom>
-          <div className="pic-work pic5-work"></div>
+          <div className="parent-work-pic">
+            <div className="pic-work pic5-work">
+              <div className="red-overly">
+                <div className="caption-work">
+                  <h2>Appartments</h2>
+                  <p>APPARTMENTS</p>
+                </div>
+              </div>
+            </div>
+          </div>
         </Zoom>
       </section>
       <br />
